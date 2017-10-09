75604a5608a438da5bb5e62c6b8f2d4c44150931
Taesung Kwon <taesung.k95@gmail.com>
2017-10-09 22:17:51 +0900

    dsdkfksdas

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,32 @@
 #include "utils.h"
+#include "string.h"
+#include "stdlib.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char* token = NULL;
+	char cm[] = "";
+	char cp[] = " \t\n";
+	char *arrs[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","",""};
+	char *p = arrs[0];
+	char **q = &p;
+	int i=0;
+	
+	strcpy(cm,command);
+
+	
+	token = strtok(cm,cp);
+
+
+	while( token != NULL)
+	{
+		strcpy(p+i,token);
+		i++;
+		token = strtok(NULL,cp);
+		
+	}
+	*argc = i;
+	
+	argv = &q;
 }
